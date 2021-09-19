9bde2d73bad8c80844e5d13159dd2d46fa0236be
DuhaAbuGhazleh <duhamahmoud0@gmail.com>
2021-09-20 02:28:21 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios';
-import OwnerBooks from './components/OwnerBooks';
+//import OwnerBooks from './components/OwnerBooks';
  class App extends Component {
 constructor(props){
   super(props);
@@ -9,19 +9,19 @@ constructor(props){
   }
 }
 
-componentDidMount=async()=>{
-  let data1= await axios.get(`${process.env.REACT_APP_BACKEND_SERVER}/Book`).then(response=>{
-    this.setState({
-      databook:data1.data
-    })
-  })
-}
+// componentDidMount=async()=>{
+//   let data1= await axios.get(`${process.env.REACT_APP_BACKEND_SERVER}/Book`).then(response=>{
+//     this.setState({
+//       databook:data1.data
+//     })
+//   })
+// }
 
   render() {
     return (
      <>
         <h1>Can of Books</h1>
-    {
+    {/* {
       this.state.data.map(item=>{
         return<OwnerBooks
         title={item.title}
@@ -29,7 +29,7 @@ componentDidMount=async()=>{
         status={item.status}
         />
       })
-    }
+    } */}
       </>
     
     )
@@ -37,4 +37,3 @@ componentDidMount=async()=>{
 }
 
 export default App
-
